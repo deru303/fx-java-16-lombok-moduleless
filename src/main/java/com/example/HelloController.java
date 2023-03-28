23ac4ec711d0b91bf6681300d223580a255b4643
Daniel Rogowski <daniel.rogowski@onet.pl>
2023-03-28 15:22:01 +0200

    Initial commit

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import lombok.Data;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
